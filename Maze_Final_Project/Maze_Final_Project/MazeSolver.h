a8d397b238bf922a8e2bd4c216c6b388f48628ca
Habiba Mohamed <habiba2405626@miuegypt.edu.eg>
2026-04-06 04:39:22 +0200

    Add files via upload

---
+++ Maze_Final_Project/Maze_Final_Project/MazeSolver.h
@@ -0,0 +1,17 @@
+#ifndef MAZESOLVER_H
+#define MAZESOLVER_H
+
+class Mazesolver {
+
+    bool success;
+    int totalCost;
+    int nodesExpanded;
+    double timeTakenMs;
+    //MyVector<Point> path;
+
+
+};
+
+
+
+#endif
